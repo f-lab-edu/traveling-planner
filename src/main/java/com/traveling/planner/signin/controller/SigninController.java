689f1b6e719f75d090a451cdf3c7571c547494f7
thkwon <thkwon@nkia.co.kr>
2020-12-14 10:55:13 +0900

    프로젝트 재 수정

--- src/main/java/com/traveling/planner/signin/controller/SigninController.java
+++ src/main/java/com/traveling/planner/signin/controller/SigninController.java
@@ -3,15 +3,17 @@ package com.traveling.planner.signin.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Map;
 
 @Controller
 public class SigninController {
 
-    @RequestMapping(value="test.do", method = RequestMethod.GET)
-    public ModelAndView test(ModelAndView mav) {
-        mav.setViewName("index");
-        return mav;
+    @RequestMapping(value = "signin", method = RequestMethod.POST)
+    public String signin(ModelAndView mav, @RequestParam Map<String, String> info) {
+        return info.toString();
     }
 
 }
---
+++ src/main/java/com/traveling/planner/vo/UsersVO.java
@@ -0,0 +1,24 @@
+package com.traveling.planner.vo;
+
+public class UsersVO {
+    private String id;
+    private String password;
+    private String name;
+    private String email;
+
+    public String getId() {
+        return id;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
