7e77db210638360ec930f24651bb2bd1d3cbc7fa
denyok1995 <denyok1995@gmail.com>
2021-03-14 13:04:31 +0900

    어노테이션의 수정, 의미없는 주석, static 변수의 삭제, print 메서드 삭제

--- src/main/java/com/traveling/planner/signin/controller/SigninController.java
+++ src/main/java/com/traveling/planner/signin/controller/SigninController.java
@@ -4,32 +4,22 @@ import com.traveling.planner.dto.UsersDto;
 import com.traveling.planner.signin.Sha256;
 import com.traveling.planner.signin.service.SigninService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class SigninController {
-
-    public static boolean flag = false;
-
-    @Autowired(required = false)
+    @Autowired
     SigninService signinService;
 
-    //postman으로 검증해야한다.
     @RequestMapping(value = "/signin", method = RequestMethod.POST)
-    //@ResponseBody
     public String signin(@Validated @RequestBody UsersDto dto, BindingResult bindingResult) {
         String sha = Sha256.encrypt(dto.getPassword());
         dto.setPassword(sha);
-        if (flag) {
-            System.out.println(bindingResult);
-            System.out.println(sha);
-        }
         signinService.signin(dto);
         return dto.toString();
     }
